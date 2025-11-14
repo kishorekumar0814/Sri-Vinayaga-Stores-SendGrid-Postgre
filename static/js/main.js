3e5ad7c3473730367faf5f2bb9b1ae02aabea19d
kishorekumar0814 <kishorekumar1409@gmail.com>
2025-11-14 18:54:35 +0530

    Ready for Render deployment

--- static/js/main.js
+++ static/js/main.js
@@ -11,3 +11,15 @@ function cardPop(el){
   el.classList.add('pop');
   setTimeout(()=> el.classList.remove('pop'), 400);
 }
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const toggleBtn = document.querySelector(".nav-toggle");
+    const sidenav = document.querySelector(".sidenav");
+
+    if (toggleBtn && sidenav) {
+        toggleBtn.addEventListener("click", () => {
+            sidenav.classList.toggle("open");
+        });
+    }
+});
